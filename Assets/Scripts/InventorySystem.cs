ba4869cba2d487057968f03f4d178e556f075e99
sonergand <vavikonstans@gmail.com>
2024-10-17 13:40:37 -0500

    Inventory 1
    item moves wrong

---
+++ Assets/Scripts/InventorySystem.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventorySystem : MonoBehaviour
+{
+
+    public static InventorySystem Instance { get; set; }
+
+    public GameObject inventoryScreenUI;
+    public bool isOpen;
+
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+
+
+    void Start()
+    {
+        isOpen = false;
+    }
+
+
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Tab) && !isOpen)
+        {
+
+            Debug.Log("open inv");
+            inventoryScreenUI.SetActive(true);
+            Cursor.lockState = CursorLockMode.None;
+            isOpen = true;
+
+        }
+        else if (Input.GetKeyDown(KeyCode.Tab) && isOpen)
+        {
+            Debug.Log("close inv");
+            inventoryScreenUI.SetActive(false);
+            Cursor.lockState = CursorLockMode.Locked;
+            isOpen = false;
+        }
+    }
+
+}
--- Assets/Scripts/MouseMovement.cs
+++ Assets/Scripts/MouseMovement.cs
@@ -19,6 +19,7 @@ public class MouseMovement : MonoBehaviour
 
     void Update()
     {
+        if (InventorySystem.Instance.isOpen) return;
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
